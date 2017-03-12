5bf16478b09f9a5dbaaa14b19ec01e491afbb5ed
Tian Zhi Wang <wtianzhi@gmail.com>
2017-03-12 17:09:41 -0600

    Add warning to login page

--- src/actions/index.js
+++ src/actions/index.js
@@ -111,6 +111,13 @@ function logIn(user) {
   };
 }
 
+function logInFail(user) {
+  return {
+    type: types.LOGGED_IN_FAILED,
+    user
+  };
+}
+
 export function attempLogin(username, password) {
   return function(dispatch) {
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
@@ -121,7 +128,7 @@ export function attempLogin(username, password) {
       }
     }).then(res => {
       if (!res.ok) {
-        return Promise.reject();
+        return Promise.reject(res);
       }
       return res;
     })
@@ -133,7 +140,11 @@ export function attempLogin(username, password) {
       }));
     })
     .catch(err => {
-      console.log('Invalid login credentials');
+      console.log(err);
+      return dispatch(logInFail({
+        ...err,
+        password
+      }));
     });
   };
 }
