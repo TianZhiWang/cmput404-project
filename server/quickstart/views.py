b06b8f6626c80b9ebf5316487dd796e975d90435
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-03-25 20:46:52 -0600

    Added some error handling

--- server/quickstart/views.py
+++ server/quickstart/views.py
@@ -161,11 +161,10 @@ class CommentList(APIView, PaginationMixin):
 
             try:
                 req = requests.post(url, auth=requests.auth.HTTPBasicAuth(node.username, node.password), data=json.dumps(request.data), headers={'Content-Type': 'application/json'})
-                if req.status_code != requests.codes.ok:
-                    raise Exception(req.text)
+                req.raise_for_status()
             except Exception as e:
                 print("Other server is down or maybe we don't have the right node")
-                print(e)
+                print(str(e))
                 return Response(status=500)
 
         #TODO: Check if they have permission to add comment (i.e. they can see the post)
@@ -311,8 +310,10 @@ class FollowingRelationshipList(APIView):
                 url = node.url + 'friendrequest/'
                 try:
                     req = requests.post(url, auth=requests.auth.HTTPBasicAuth(node.username, node.password), data=json.dumps(request.data), headers={'Content-Type': 'application/json'})
-                except:
-                    print("Other server is down or maybe we don't have the right node")
+                    req.raise_for_status()
+                except Exception as e:
+                    print("Exception occurred in friendrequest")
+                    print(str(e))
                     return Response(status=500)
 
                 author = get_object_or_404(Author, pk=get_author_id_from_url(author_data))
@@ -378,6 +379,7 @@ class AllPostsAvailableToCurrentUser(APIView,PaginationMixin):
                 url = node.url + 'author/posts/'
                 try:
                     req = requests.get(url, auth=requests.auth.HTTPBasicAuth(node.username, node.password))
+                    req.raise_for_status()
                     unfilteredForeignPosts = req.json()['posts']
                     
                     for post in unfilteredForeignPosts:
@@ -387,8 +389,10 @@ class AllPostsAvailableToCurrentUser(APIView,PaginationMixin):
                             serializedPosts.append(post)
                         elif post['visibility'] == 'PRIVATE' and (str(author.id) in post['visibleTo']):
                             serializedPosts.append(post)
-                except:
-                    print("Other server is down or giving bad data")
+                except Exception as e:
+                    print("Exception occurred in author/posts")
+                    print(str(e))
+                    return Response(status=500)
             
             return Response(serializedPosts)
 
