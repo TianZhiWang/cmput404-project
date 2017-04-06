89a26f9e041b6f720c96c84639f07dc9ca74129e
Kyle Carlstrom <kylecarlstrom@gmail.com>
2017-04-05 21:10:47 -0600

    Clean up networking code a bit

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,53 @@
+import {URL_PREFIX} from '../constants';
+
+
 function getUUIDFromId(id) {
   return /author\/([a-zA-Z0-9-]+)\/?$/.exec(id, 'g')[1];
 }
 
+/*
+* Makes a network request and returns the result if any
+* Method: One of 'GET', 'POST', and 'PUT'
+* Headers: {} any additional headers, default is only basic auth
+* Body: {} expects an object containing the body
+* Returns a promise with the response in JSON
+*/
+function basicAuthFetch(method, path, user, body) {
+  const request = {
+    method,
+    headers: {
+      // Written by unyo (http://stackoverflow.com/users/2077884/unyo http://stackoverflow.com/a/35780539 (MIT)
+      'Authorization': `Basic ${btoa(`${user.username}:${user.password}`)}`
+    }
+  };
+  if (body) {
+    request.headers = Object.assign(request.headers, {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
+    request.body = JSON.stringify(body);
+  }
+
+  return fetch(`${URL_PREFIX}${path}`, request)
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res);
+      }
+      return res;
+    })
+    .then(res => {
+      return method !== 'DELETE' ? res.json() : res;
+    })
+    .catch(error => {
+      console.error('START ERROR MESSAGE >>>>>>>>>>>>>>');
+      console.error('Caught error while performing request');
+      console.error('Request: ', request);
+      console.error('Error: ', error);
+      console.error('<<<<<<<<<<<<<<<<<< END ERROR MESSAGE');
+    });
+}
+
 export {
-    getUUIDFromId
+  getUUIDFromId,
+  basicAuthFetch
 };
